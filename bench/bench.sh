8d281a1ebc71d8586785a82e4990d8fa4718fa23
Matts966 <Matts966@users.noreply.github.com>
2020-01-10 15:51:43 +0900

    Fixed minor bugs in bench script

--- bench/bench.sh
+++ bench/bench.sh
@@ -16,22 +16,22 @@ for ((i=1; i<=5; i++)); do
     git checkout master
     dir=$tmpD/with-cache/full
     (cd .. && { time make build-without-cache ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 
     git checkout b1c7b3c2fb6bf5297bbbb4b5e75a23b0bc8d7c24
     dir=$tmpD/with-cache/kernel
     (cd .. && { time make build ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 
     git checkout 40ebad368553e1798dbaa56802ecfe44c0130402
     dir=$tmpD/with-cache/user
     (cd .. && { time make build ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 
     git checkout 9c524750a3cbfbfedd1d86629280c35d4402af5e
     dir=$tmpD/with-cache/middleware
     (cd .. && { time make build ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 done
 
 # simple
@@ -39,22 +39,22 @@ for ((i=1; i<=5; i++)); do
     git checkout 2af8f14ec2cafa1e40ec665ae9e538279ec8ba1d
     dir=$tmpD/simple/full
     (cd .. && { time make build-without-cache ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 
     git checkout d08937d85754fd7fb3f8f75ba3f29ac95b9a99c8
     dir=$tmpD/simple/kernel
     (cd .. && { time make build ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
     
     git checkout dd9e64b44c6dd937e737f789b9672bd732e3edc2
     dir=$tmpD/simple/user
     (cd .. && { time make build ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 
     git checkout 8ed83ee1ba89e7a46a6b5d7a3e59d4d16e48281e
     dir=$tmpD/simple/middleware
     (cd .. && { time make build ; } 2> $dir/$i \
-        && cat $dir/i | tail -n1 | $dir/result.txt)
+        && cat $dir/$i | tail -n1 >> $dir/result.txt)
 done
 
 echo "temporary directory: $tmpD"
